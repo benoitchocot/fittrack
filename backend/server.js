7fe9d9585608299ccc01443df9a6974f683ee2bb
bonoit <benoit.chocot@gmail.com>
2025-05-24 18:43:00 +0200

    fix cors error ?

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,26 @@ const authRoutes = require("./routes/auth");
 const app = express();
 const PORT = 3001;
 
-app.use(cors());
+const allowedOrigins = ['https://muscu.chocot.be'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
+// Si tu utilises des requêtes avec Content-Type: application/json
 app.use(bodyParser.json());
 
 app.use("/templates", templatesRoutes);
 app.use("/history", historyRoutes);
 app.use("/auth", authRoutes);
+app.options('*', cors()); // pour répondre aux preflight requests
 
 app.listen(PORT, () => {
   console.log(`✅ API listening at http://localhost:${PORT}`);
