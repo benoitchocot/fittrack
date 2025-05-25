4d2c08645c2f3410c5fd8b2f8229b2e886c09c9a
bonoit <benoit.chocot@gmail.com>
2025-05-25 15:32:35 +0200

    fi cors

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,5 @@
 const BASE_URL = 'https://apimuscu.chocot.be/';
 
-
-export const JWT_SECRET = 'ton_secret_pour_jwt_super_secret'; // change ça en prod !
-
-
-export default BASE_URL;
+module.exports = {
+  BASE_URL,
+};
