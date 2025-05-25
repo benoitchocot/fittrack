975ca86a6e2333dfc28db4b51779db5ff891f2bd
bonoit <benoit.chocot@gmail.com>
2025-05-25 15:29:27 +0200

    fix cors

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config"); // Importer JWT_SECRET
+const JWT_SECRET = require("../config"); // Importer JWT_SECRET
 
 
 function authenticate(req, res, next) {
