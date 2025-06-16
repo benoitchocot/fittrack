4874dc1593636b20c617f74a03ee74bac4ba41e5
bonoit <benoit.chocot@gmail.com>
2025-06-16 18:45:33 +0200

    tg

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-// const BASE_URL = 'https://apimuscu.chocot.be/';
- const BASE_URL = 'http://localhost:3001/';
+ const BASE_URL = 'https://apimuscu.chocot.be/';
+// const BASE_URL = 'http://localhost:3001/';
 
 export default BASE_URL;
