0c51f3290d60283c91358bb0dbccad8ed94ccf74
Benoit Chocot <61710890+benoitchocot@users.noreply.github.com>
2025-09-13 10:31:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,6 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
   },
   plugins: [
     react(),
