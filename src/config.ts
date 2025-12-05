a246b0914dcae9395ca682cb894cfafd7d679ff2
benoit <benoit.chocot@gmail.com>
2025-12-05 19:22:55 +0100

    mauvaise adresse

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 // For local dev (npm run dev): VITE_BASE_URL=http://localhost:3001/
 // For production (nginx proxy): VITE_BASE_URL=https://apimuscu.chocot.be/
 // In docker-compose, nginx proxies /api to backend, so use /api
-const BASE_URL = 'http://localhost:3001/';
+const BASE_URL = 'https://apimuscu.chocot.be/';
 export default BASE_URL;
